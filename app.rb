7e59dd07c2c749402c9592bb3e73b06161b67b83
keith(mac) <ka33145@googlemail.com>
2017-09-13 18:05:17 +0100

    Moved from app to index.erb

--- app.rb
+++ app.rb
@@ -2,21 +2,7 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working'
-    '
-    <form method="post" action="/names" a>
-      <div style="padding:5px">
-      <label for=player1>Player1 Name</label>
-      <input type="text" name="player1">
-      </div>
-      <div style="padding:5px">
-      <label for=player1>Player2 Name</label>
-      <input type="text" name="player2">
-      </div>
-      <div style="padding:5px">
-      <input type="submit" value="submit" style="padding:5px">
-      </div>
-    </form>'
+    erb :index
   end
 
   post '/names' do
