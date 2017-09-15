2ebafa9077d005a4fdfdc94dfa6404899e15e160
Joe Ingram <josephcingram@hotmail.co.uk>
2017-09-15 15:03:08 +0100

    add attack function and test

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -3,6 +3,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
     sign_in_and_play
-    expect(page).to have_content 'Joe vs. Aga'
+    expect(page).to have_content 'Joe vs. Nero'
   end
 end
--- spec/features/player_1_attack_spec.rb
+++ spec/features/player_1_attack_spec.rb
@@ -1,7 +1,7 @@
 feature 'attack' do
   scenario 'should attack P2' do
     sign_in_and_play
-    click_link 'Attack'
-    expect(page).to have_content 'Joe attacked Aga'
+    click_button 'Attack!!!!!!!'
+    expect(page).to have_content 'Joe attacked Nero'
   end
 end
